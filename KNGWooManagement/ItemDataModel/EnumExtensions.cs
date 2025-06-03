7a2327e096f8a4cebf79a7967ccf3fda60ec301b
Jennavieve Williams <140333667+jen604williams@users.noreply.github.com>
2025-06-03 21:00:36 +0200

    Add Enum extenstions to get the display name - this needs to be moved to a better folder

---
+++ KNGWooManagement/ItemDataModel/EnumExtensions.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+public static class EnumExtensions
+{
+    public static string GetDisplayName(this Enum enumValue)
+    {
+        var displayAttribute = enumValue.GetType()
+            .GetMember(enumValue.ToString())
+            .FirstOrDefault()?
+            .GetCustomAttribute<DisplayAttribute>();
+
+        return displayAttribute?.Name ?? enumValue.ToString();
+    }
+}
